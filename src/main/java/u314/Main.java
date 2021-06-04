65abadccaaa7a106228884c4c4d72b5f3b27b18b
svubax <svubax@ya.ru>
2021-06-04 12:38:00 +0300

    Add NewClass

--- src/main/java/u314/Main.java
+++ src/main/java/u314/Main.java
@@ -8,32 +8,35 @@ public class Main {
     public static void main(String[] args) {
         String url = "http://91.241.64.178:7081/api/users/";
         RestTemplate restTemplate = new RestTemplate();
-        User user = new User(3, "James", "Brown", 5);
+        User user = new User(3, "James", "Brown", 88);
         ObjectMapper mapper = new ObjectMapper();
-        String password = "";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        String code = "";
         try {
             ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-            String jsessionid = response.getHeaders().get(HttpHeaders.SET_COOKIE).get(0).split("; ")[0];
+            String jsessionid = response.getHeaders().get(HttpHeaders.SET_COOKIE).get(0);
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
             headers.add(HttpHeaders.COOKIE, jsessionid);
             HttpEntity<String> entity = new HttpEntity<>(mapper.writeValueAsString(user), headers);
             response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
-            password += response.getBody();
+            code += response.getBody();
+            System.out.println("1-я часть кода: " + response.getBody());
 
-            user.setName("James");
-            user.setLastName("Brown");
+            user.setName("Thomas");
+            user.setLastName("Shelby");
             entity = new HttpEntity<>(mapper.writeValueAsString(user), headers);
             response = restTemplate.exchange(url, HttpMethod.PUT, entity, String.class);
-            password += response.getBody();
+            code += response.getBody();
+            System.out.println("2-я часть кода: " + response.getBody());
 
             entity = new HttpEntity<>(headers);
             response = restTemplate.exchange(url + user.getId(), HttpMethod.DELETE, entity, String.class);
-            password += response.getBody();
+            code += response.getBody();
+            System.out.println("3-я часть кода: " + response.getBody());
         } catch (Exception e){
             e.printStackTrace();
         }
-        System.out.println(password);
+        System.out.println("Код: " + code);
     }
 }
---
+++ src/main/java/u314/NewClass.java
@@ -0,0 +1,4 @@
+package u314;
+
+public class NewClass {
+}
